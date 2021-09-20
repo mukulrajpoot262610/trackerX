4f7eaaa028635abe906d19345542653693f0e83d
mukul262610 <mukul@stargate-inc.comgit config --global user.email mukul@stargate-inc.com>
2021-09-20 14:41:59 +0530

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const path = require('path');
 const cors = require('cors')
 
 dotenv.config()
@@ -19,6 +20,17 @@ app.use('/api/user', require('./routes/userRoutes'))
 app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/data', require('./routes/dataRoutes'))
 
+if (node_env === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Hello World")
+    })
+}
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
 })
